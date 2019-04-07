d69a6491aca2d1c685c7b6351cb650c542c10762
Alihoussene Silahi <silahi.alihoussene@gmail.com>
2019-04-07 16:41:00 +0100

    Mise à jour de la classe Face.java permettant d'obtenir une face -> Face.java

--- Face.java
+++ Face.java
@@ -1,74 +1,152 @@
-
-import com.sun.j3d.utils.geometry.*;
-import javax.media.j3d.*; 
-public class Face extends House{
-	  protected TransformGroup faceGroup;
-	  protected QuadArray face;
-	  protected Shape3D shape;
-	  protected Appearance app;
-	  private float x,y,z;
-	  public Face(){
-	  	    faceGroup = new TransformGroup();
-            face      = new QuadArray(House.VERTEX_COUNT,GeometryArray.COORDINATES| GeometryArray.TEXTURE_COORDINATE_2);
-            shape     = new Shape3D();
-             shape.removeGeometry(0);
-            shape.setGeometry(face); 
-            faceGroup.addChild(shape);
-	  }
-
-	    public Face(int faceValue , float x , float y , float z){
-	  	     this();
-	  	     this.x = x;
-	  	     this.y = y;
-	  	     this.z = z;
-	  	     setFaceCoordinate(face , faceValue , x,y,z); 
-	  	     setTextureFaceCoord(face , 4);
-	  } 
-	  public Face(String fileImage , Canvas3D canvas){
-            faceGroup = new TransformGroup();
-            face      = new QuadArray(4,GeometryArray.COORDINATES| GeometryArray.TEXTURE_COORDINATE_2);
-            shape     = new Shape3D();
-            shape.setGeometry(face); 
-            faceGroup.addChild(shape);
-            app = new Apparence(fileImage , canvas);
-            shape.setAppearance(app);
-	  } 
  
+ import com.sun.j3d.utils.geometry.*;
+import javax.media.j3d.*;
+import javax.vecmath.*;
 
-	  public Face(String fileImage , Canvas3D canvas , int faceValue , float x , float y , float z ){
-           this(fileImage , canvas);
-             this.x = x;
-	  	     this.y = y;
-	  	     this.z = z;
-           setFaceCoordinate(face , faceValue , x,y,z);
-           setTextureFaceCoord(face , 4);
-	  }	
-        // Door face and Cubic face
-	  public Face(String fileImage , int faceValue, float x , float y , float z){
-	  	     this.x = x;
-	  	     this.y = y;
-	  	     this.z = z;	  	     
-	  	     if(faceValue == FACE_DOOR){
-	  	     	   face      = new QuadArray(12,GeometryArray.COORDINATES| GeometryArray.TEXTURE_COORDINATE_2);	  	     	     	     	   
-                   setFaceCoordinate(face ,FACE_DOOR, x,y,z); 
-                   setTextureFaceCoord(face , 12);
-	  	     }
+/**
+*@author Alhoussene
+*date 07/04/18
+*@version 1.2
+**/
 
-	  	      if(faceValue == CHKOMBE){
-	  	     	   face      = new QuadArray(24,GeometryArray.COORDINATES| GeometryArray.TEXTURE_COORDINATE_2);	  	     	     	     	   
-                   setFaceCoordinate(face ,CHKOMBE, x,y,z); 
-                   setTextureFaceCoord(face , 24);
-	  	     }
-             
+public  class Face { 
+   public static final int BOTTOM = 0;
+   public static final int TOP    = 1;   
+   public static final int RIGHT  = 2;
+   public static final int FRONT  = 3;
+   public static final int LEFT   = 4;
+   public static final int BACK   = 5;
+   public static final int BACK_ROOF  = 6;
+   public static final int FRONT_ROOF = 7;
+	protected QuadArray face;
+	protected TransformGroup quadGroup;
+	protected Shape3D shape;
+	protected Appearance app;
+    public Face(String fileImage , int faceValue ,float x , float y, float z){
+             app = new Apparence(fileImage , null);
+             face= new QuadArray(4 , QuadArray.COORDINATES|QuadArray.TEXTURE_COORDINATE_2);
+             shape = new Shape3D(face , app);
+             setQuadCoordinates(faceValue,x,y,z);
+             setQuadTextureCoords();
+             quadGroup = new TransformGroup();
+             quadGroup.addChild(shape); 
+    }
 
-             app       = new Apparence(fileImage , null);	
-             shape     = new Shape3D(face , app);
-             faceGroup = new TransformGroup();
-             faceGroup.addChild(shape); 
-	  }
-
-	  public float getX(){ return x;}
-	  public float getY(){ return y;}
-	  public float getZ(){ return z;}
-	 
+    private void  setQuadCoordinates( int faceValue, float x , float y , float z){   
+           Point3f point = new Point3f();
+          // Generation bootom face coordinates
+          if(faceValue == BOTTOM){
+          	 point.set(x,0,z);
+             face.setCoordinate(0 , point);
+             point.set(x,0,-z);
+             face.setCoordinate(1 , point);
+             point.set(-x,0,-z);
+             face.setCoordinate(2 , point);
+             point.set(-x,0,z);
+             face.setCoordinate(3 , point);
+          }      
+          // Generation top face coordinates
+          else if(faceValue == TOP){
+          	 point.set(x,y,z);
+             face.setCoordinate(0 , point);
+             point.set(x,y,-z);
+             face.setCoordinate(1 , point);
+             point.set(-x,y,-z);
+             face.setCoordinate(2 , point);
+             point.set(-x,y,z);
+             face.setCoordinate(3 , point);
+          }     
+           // Generation right face coordinates
+          else if(faceValue == RIGHT){
+          	 point.set(x,0,z);
+             face.setCoordinate(0 , point);
+             point.set(x,y,z);
+             face.setCoordinate(1 , point);
+             point.set(x,y,-z);
+             face.setCoordinate(2 , point);
+             point.set(x,0,-z);
+             face.setCoordinate(3 , point);
+          }       
+           // Generation front face coordinates
+          else if(faceValue == FRONT){
+          	 point.set(x,0,-z);
+             face.setCoordinate(0 , point);
+             point.set(x,y,-z);
+             face.setCoordinate(1 , point);
+             point.set(-x,y,-z);
+             face.setCoordinate(2 , point);
+             point.set(-x,0,-z);
+             face.setCoordinate(3 , point);
+          }
+           // Generation left face coordinates
+          else if(faceValue == LEFT){
+          	 point.set(-x,0,z);
+             face.setCoordinate(0 , point);
+             point.set(-x,0,-z);
+             face.setCoordinate(1 , point);
+             point.set(-x,y,-z);
+             face.setCoordinate(2 , point);
+             point.set(-x,y,z);
+             face.setCoordinate(3 , point);
+          } 
+           // Generation back face coordinates
+          else if(faceValue == BACK){
+          	 point.set(x,0,z);
+             face.setCoordinate(0 , point);
+             point.set(x,y,z);
+             face.setCoordinate(1 , point);
+             point.set(-x,y,z);
+             face.setCoordinate(2 , point);
+             point.set(-x,0,z);
+             face.setCoordinate(3 , point);
+          } 
+          // Generation du toit de droite 
+          else if(faceValue == BACK_ROOF) {
+             point.set(-x,y,z);
+             face.setCoordinate(0 , point);
+             point.set(x,y,z);
+             face.setCoordinate(1 , point);
+             point.set(x,y+(y/2),0);
+             face.setCoordinate(2 , point);
+             point.set(-x,y+(y/2),0);
+             face.setCoordinate(3 , point);
+          }
+           // Generation du toit de gauche
+          else if(faceValue == FRONT_ROOF) {
+             point.set(-x,y,-z);
+             face.setCoordinate(0 , point);
+             point.set(x,y,-z);
+             face.setCoordinate(1 , point);
+             point.set(x,y+(y/2),0);
+             face.setCoordinate(2 , point);
+             point.set(-x,y+(y/2),0);
+             face.setCoordinate(3 , point);
+          }  
+          
+           // Default Generation coordinates Face -> Front Face
+          else {
+          	 point.set(-x,0,z);
+             face.setCoordinate(0 , point);
+             point.set(x,0,z);
+             face.setCoordinate(1 , point);
+             point.set(x,y,z);
+             face.setCoordinate(2 , point);
+             point.set(-x,y,z);
+             face.setCoordinate(3 , point);
+          }  
+                    
+   } 
+    private void setQuadTextureCoords(){
+    	      // Face 1
+    	      TexCoord2f texCoord = new TexCoord2f();              
+              texCoord.set(0f ,0f); 
+              face.setTextureCoordinate(0,0 , texCoord);              
+              texCoord.set(1f ,0f); 
+              face.setTextureCoordinate(0,1 , texCoord);               
+              texCoord.set(1f ,1);
+              face.setTextureCoordinate(0,2 , texCoord);               
+              texCoord.set(0f ,1f);
+              face.setTextureCoordinate(0,3 , texCoord); 
+          
+    }
 }
